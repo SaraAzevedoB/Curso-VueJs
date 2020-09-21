06d837ec992d05fc631410a953bb5e317b02afa0
Sara <benedictosara@gmail.com>
2020-09-21 18:56:31 -0300

    desafio1

---
+++ Desafio1/app.js
@@ -0,0 +1,19 @@
+new Vue({
+    el: '#desafio',
+    data: {
+        nome: 'Sara Azevedo',
+        idade: 30,
+        imagem: 'http://files.cod3r.com.br/curso-vue/vue.jpg'
+        
+    },
+   methods:{
+       idadeVezes3() {
+           return this.idade * 3
+       },
+       random() {
+           return Math.random()
+       }
+   }
+
+    
+})
